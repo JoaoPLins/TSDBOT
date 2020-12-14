61c934d7814b9cd8b456f67540457441113aa4f2
JoaoPLins <60204831+JoaoPLins@users.noreply.github.com>
2020-12-14 11:59:21 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export let config = { 
-    "token" : "NzI0MTA0OTgxMTg3MTMzNTQy.Xu7VUA.h0oEXcl794ixF-AsJZBG5wCllK4" , 
+    "token" : "" , 
     "prefix" : "!",
     "commands" : [
         "TestCommand"
